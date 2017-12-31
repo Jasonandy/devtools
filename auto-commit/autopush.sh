10562cca95eecbd28e0deb9a89c547c86a6bf6a9
Jasonandy <jasonandy@hotmail.com>
2017-12-31 13:18:27 +0800

    :tada:

--- auto-commit/autopush.sh
+++ auto-commit/autopush.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo "---开始自动提交数据-----"
-cd /data/git-auto-commit && git pull && /usr/bin/node add.js && git commit -a -m 'git auto commit' && git push origin master 
-
+cd /data/devtools/auto-commit && git pull && /usr/bin/node add.js && git commit -a -m ':tada:' && git push origin master 
 echo "-----Push完成-------:·"
