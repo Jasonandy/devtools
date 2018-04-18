33a4724cce574183e3126b023cf38df615afd130
root <root@host.localdomain>
2018-04-18 03:17:33 -0400

    :tada:

--- auto-commit/push.sh
+++ auto-commit/push.sh
@@ -1,4 +1,4 @@
 echo "--------开始push数据---------"
-git pull && /usr/bin/node add.js && git commit -a -m ':tada:' && git push origin master
+git pull && node add.js && git commit -a -m ':tada:' && git push origin master
 echo "--------push完成-------------"
 
