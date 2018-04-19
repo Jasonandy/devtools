9fca038460175c1c2f10a07f699d545dc378f298
Jasonandy <jason2andy@gmail.com>
2018-04-19 05:12:10 -0400

    :tada:

--- auto-commit/autopush.sh
+++ auto-commit/autopush.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 echo "---开始自动提交数据-----"
 
-File_Dir="/data/devtools/auto-commit"
+File_Dir=`pwd`
+
+echo "当前的目录的位置为" ${File_Dir}
 
 cd ${File_Dir} && git pull && node add.js && git commit -a -m ':tada:' && git push origin master 
+
 echo "-----Push完成-------:·"
